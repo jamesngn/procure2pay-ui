4fb16f26728be955ccd4d954c43c6555b6d9ed3b
jamesnguyennm <jamesnguyennm@gmail.com>
2024-07-19 20:18:51 +1000

    chore: Fix typo in RequisitionPage component

--- src/pages/requisition.page/index.tsx
+++ src/pages/requisition.page/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import { HeaderTitle } from '@/layout/app-layout';
 
-import RequisitonTable from './components/requistion-table';
+import RequisitionTable from './components/requisition-table';
+
 
 const RequisitionPage: React.FC = () => {
   return (
     <>
       <HeaderTitle>Requisition</HeaderTitle>
-      <RequisitonTable />
+      <RequisitionTable />
     </>
   );
 };
